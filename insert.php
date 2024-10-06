47d031e226923150db7d957115d12d6896a3d22f
momo0315 <momo110625@gmail.com>
2024-10-06 14:59:11 +0900

    PHPで作成した問い合わせフォーム

---
+++ insert.php
@@ -0,0 +1,29 @@
+<?php
+
+mb_internal_encoding("utf8");
+$pdo=new PDO("mysql:dbname=lesson1;host=localhost;","root","");
+$sql="insert into contactform(name,mail,age,comments) values(?,?,?,?)";
+$stmt=$pdo->prepare($sql);
+$stmt->bindvalue(1,$_POST['name']);
+$stmt->bindvalue(2,$_POST['mail']);
+$stmt->bindvalue(3,$_POST['age']);
+$stmt->bindvalue(4,$_POST['comments']);
+$stmt->execute();
+
+?>
+
+<!doctype HTML>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>お問合わせフォームを作る</title>
+        <link rel="stylesheet" type="text/css" href="style2.css">
+    </head>
+    <body>
+     <h1>お問合わせフォーム</h1>
+     <div class="confirm">
+         <p>お問合わせ有り難うございました。<br>
+            3営業日以内に担当者よりご連絡差し上げます。</p>
+     </div>
+    </body>
+</html>
